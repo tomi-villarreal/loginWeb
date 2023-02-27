88a7ba94d8767e3edf5118fc56ed6ca54c8e5221
Tomás Villarreal <toncho196kaifer@gmail.com>
2023-02-27 14:45:40 -0300

    Version 1.1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const button = document.querySelector("#button");
 const texto = document.querySelector("#input")
+const message = document.getElementById("message");
+const messageError = document.getElementById("message-e");
 
 function validatePassoword(text){
     if(text == "asd"){
@@ -11,10 +13,18 @@ function validatePassoword(text){
 button.addEventListener("click", ()=>{
     const textoInput = document.getElementById("input");
     if(validatePassoword(textoInput.value)){
-        alert("Contraseña correcta");
+        // message.classList.toggle("message-color");
+        message.style.cssText = "display: flex";
     }else{
         textoInput.value = "";
-        alert("Contraseña incorrecta");
-    }
+        messageError.style.cssText = "display: flex";
+    }   
 })
 
+message.addEventListener("click", ()=>{
+    message.style.cssText = "display: none";
+})
+
+messageError.addEventListener("click", ()=>{
+    messageError.style.cssText = "display: none";
+})
